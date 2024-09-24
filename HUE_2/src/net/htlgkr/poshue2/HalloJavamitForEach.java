c3889f153d44618f41023db8039dc3c9cdfbbae9
HauserS22081 <HauserS22081@sus.htl-grieskirchen.at>
2024-09-24 11:23:01 +0200

    second Assignment: initial code

--- HUE_2/src/net/htlgkr/poshue2/HalloJavamitForEach.java
+++ HUE_2/src/net/htlgkr/poshue2/HalloJavamitForEach.java
@@ -4,5 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HalloJavamitForEach {
-    
+
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<String>(List.of("a", "b", "c", "d", "e", "f"));
+
+        for (String element : stringList) {
+            System.out.println(element);
+        }
+
+        stringList.forEach(System.out::println);
+    }
 }
