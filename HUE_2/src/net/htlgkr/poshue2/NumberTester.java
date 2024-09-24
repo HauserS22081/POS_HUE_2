1148b3f5197ca5c68f320fb80870d2e838ba79fe
HauserS22081 <HauserS22081@sus.htl-grieskirchen.at>
2024-09-24 12:11:56 +0200

    third Assignment: initial code for methods

--- HUE_2/src/net/htlgkr/poshue2/NumberTester.java
+++ HUE_2/src/net/htlgkr/poshue2/NumberTester.java
@@ -1,14 +1,21 @@
 package net.htlgkr.poshue2;
 
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class NumberTester {
 
 
     private NumberTest oddTester;
     private NumberTest primeTester;
     private NumberTest palindromeTester;
+    private String fileName;
 
     public NumberTester(String fileName) {
-
+        this.fileName = fileName;
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
@@ -24,6 +31,47 @@ public class NumberTester {
     }
 
     public void testFile() {
+        Map<Integer, Integer> file = readInFile();
 
+        // file.forEach((key, value) -> useNumberTester(key, value));
+
+        file.forEach(this::useNumberTester);
     }
+
+    private void useNumberTester(int key, int value) {
+        switch (key){
+            case 1:
+                System.out.println(oddTester.testNumber(value) ? "EVEN" : "ODD"); return;
+            case 2:
+                System.out.println(primeTester.testNumber(value) ? "PRIME" : "NOT PRIME"); return;
+            case 3:
+                System.out.println(palindromeTester.testNumber(value) ? "PALINDROME" : "NOT PALINDROME"); 
+        }
+    }
+
+    private Map<Integer, Integer> readInFile() {
+        Map<Integer, Integer> file = new LinkedHashMap<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(new File(fileName)))){
+            String line = br.readLine();
+            line = br.readLine();
+            while(line != null){
+                String[] parts = line.split(" ");
+                file.put(Integer.parseInt(parts[0]), Integer.valueOf(parts[1]));
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error in NumberTester.readInFile - Message: " + e.getMessage());
+        }
+
+        return file;
+    }
+
+//    private void declarePrimeTester() {
+//        NumberTest oddTester = number -> (number % 2) == 0;
+//
+//        NumberTest primeTester = number -> IntMath.isPrime(number);
+//    }
+
 }
