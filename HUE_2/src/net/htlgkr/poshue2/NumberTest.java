bf8764e0c37d3966701c453e976d4dcff4fa2b5a
HauserS22081 <HauserS22081@sus.htl-grieskirchen.at>
2024-09-24 11:29:41 +0200

    thirt Assignment: initial class setup

---
+++ HUE_2/src/net/htlgkr/poshue2/NumberTest.java
@@ -0,0 +1,5 @@
+package net.htlgkr.poshue2;
+
+public interface NumberTest {
+    boolean testNumber(int number);
+}
---
+++ HUE_2/src/net/htlgkr/poshue2/NumberTester.java
@@ -0,0 +1,26 @@
+package net.htlgkr.poshue2;
+
+public class NumberTester {
+
+    //...
+
+    public NumberTester(String fileName) {
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        
+    }
+}
